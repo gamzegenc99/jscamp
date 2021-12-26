9dc06b534a3017ccc9ef05bd9108ed057e4084a1
gamzegenc99 <gamzegenc99@gmail.com>
2021-12-27 02:55:37 +0300

    JS Kamp1 Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba Kodlama.io")
+let konutKredileri=["Konut kredisi","Emlak Kredisi","Komu Konut Kredisi"] //array
+console.log("<ul>") //unorderedlisteluşturduk
+for(let i=0;i<konutKredileri.length
+    ;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")// array içinde dolaş liste elemanlarını yazdır.
+}
+
+console.log("</ul>")
